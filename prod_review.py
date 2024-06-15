eb369bccb5e3cbf2ff896423ec1fda130eb8e78a
PrabhasKalyan <146581074+PrabhasKalyan@users.noreply.github.com>
2024-06-16 00:05:19 +0530

    Update prod_review.py

--- prod_review.py
+++ prod_review.py
@@ -9,25 +9,25 @@ df=pd.read_csv('amazon.csv')
 reviews=[]
 labels=[]
 for i in range(len(df)):
-    reviews.append(df["Text"][i])
-    labels.append(df["label"][i])
-labels=np.array(labels)
-tokenizer=Tokenizer(num_words=200,oov_token="<OOV>")
-tokenizer.fit_on_texts(reviews)
-word_index = tokenizer.word_index
-seq=tokenizer.texts_to_sequences(reviews)
-pad_seq=pad_sequences(seq,padding='post',maxlen=10,truncating="post")
+    reviews.append(df["Text"][i])  #append review to the list
+    labels.append(df["label"][i])   #append label of a particular review to the list
+labels=np.array(labels)             #convert  list to array
+tokenizer=Tokenizer(num_words=200,oov_token="<OOV>")    #tokenising the words to numbers by word emebedding methodand adding OOV to the new words
+tokenizer.fit_on_texts(reviews)                         #Fiiting the tokenizer
+word_index = tokenizer.word_index                      
+seq=tokenizer.texts_to_sequences(reviews)               #converting tokens to sequences  
+pad_seq=pad_sequences(seq,padding='post',maxlen=10,truncating="post")   #adding extra tokens (usually zeros) to make all sequences in a dataset the same length
 
 model = tf.keras.Sequential([
- tf.keras.layers.Input(shape=(10,)),  
- tf.keras.layers.Embedding(10000, 16),
- tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
- tf.keras.layers.Flatten(),
- tf.keras.layers.Dense(6, activation='relu'),
- tf.keras.layers.Dense(1, activation='sigmoid')
+ tf.keras.layers.Input(shape=(10,)),  #accepting shape of review
+ tf.keras.layers.Embedding(10000, 16),  #creates an embedding layer that maps 10,000 unique integer tokens to 16-dimensional dense vectors
+ tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)), #Biderectional neural networks and LSTM
+ tf.keras.layers.Flatten(), #Converting the array to a vector to be fed into fully connected layer
+ tf.keras.layers.Dense(6, activation='relu'), 
+ tf.keras.layers.Dense(1, activation='sigmoid') #activation function
 ])
 
-model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy']) #loss is binary cross entropy as we are classifying binary classification
 model.fit(pad_seq,labels,epochs=10)
 # model.summary()
 pred_review="It is bad"
